66c75909efd69acce683bdc67acca1f99953f76b
Patrick Roos <patrick@point42.nl>
2018-06-03 15:30:09 +0200

    Add project files.

---
+++ Patterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Patterns
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("test");
+            Console.ReadKey();
+        }
+    }
+}
