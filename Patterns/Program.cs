f503666f2b0925216d0df69954be9a3f4f95bec0
Patrick Roos <patrick@point42.nl>
2018-06-06 21:18:35 +0200

    Set to starting example

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -9,13 +9,13 @@
 
         private static void RunFactoryPaternExamples()
         {
-            //Factories._1_Creating_Basic_Pizza.Example.Run();
+            Factories._1_Creating_Basic_Pizza.Example.Run();
             
             //Factories._2_Simple_Factory.Example.Run();
 
             //Factories._3_Factory_Method.Example.Run();
 
-            Factories._4_Abstract_Factory.Example.Run();
+            //Factories._4_Abstract_Factory.Example.Run();
         }
     }
 }
