d93a5b03e4fb71368f7162dfb4c4f3d5d29ae6bb
Patrick Roos <patrick@point42.nl>
2018-06-06 21:16:20 +0200

    Finished with unit tests

---
+++ Patterns.Tests/OrderNyStylePizzaExample_Constructor_Should.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Patterns.Testing._1_With_Testing;
+using Patterns.Testing._1_With_Testing.PizzaStorePizzaBuilders.Ny;
+
+namespace Patterns.Tests
+{
+    [TestClass]
+    // ReSharper disable once InconsistentNaming
+    public class OrderNyStylePizzaExample_Constructor_Should
+    {
+        private INyPizzaStorePizzaBuilder _nyPizzaStorePizzaBuilder;
+
+        [TestInitialize]
+        public void InitializeMocks()
+        {
+            _nyPizzaStorePizzaBuilder = new Mock<INyPizzaStorePizzaBuilder>().Object;
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Create_Instance_Of_Type()
+        {
+            var sut = new OrderNyStylePizzaExample(_nyPizzaStorePizzaBuilder);
+            Assert.IsInstanceOfType(sut, typeof(OrderNyStylePizzaExample));
+        }
+    }
+}
---
+++ Patterns.Tests/OrderNyStylePizzaExample_PlayOrderPizzaExample_Should.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Patterns.Testing._1_With_Testing;
+using Patterns.Testing._1_With_Testing.Pizzas;
+using Patterns.Testing._1_With_Testing.PizzaStorePizzaBuilders.Ny;
+
+namespace Patterns.Tests
+{
+    [TestClass]
+    // ReSharper disable once InconsistentNaming
+    public class OrderNyStylePizzaExample_PlayOrderPizzaExample_Should
+    {
+        private Mock<INyPizzaStorePizzaBuilder> _nyPizzaStorePizzaBuilder;
+
+        [TestInitialize]
+        public void InitializeMocks()
+        {
+            var mockPizza = new Mock<Pizza>();
+
+            _nyPizzaStorePizzaBuilder = new Mock<INyPizzaStorePizzaBuilder>();
+
+            _nyPizzaStorePizzaBuilder
+                .Setup(x => x.CreateBasicPizza(It.IsAny<PizzaType>()))
+                .Returns(_nyPizzaStorePizzaBuilder.Object);
+
+            _nyPizzaStorePizzaBuilder
+                .Setup(x => x.AddMushrooms())
+                .Returns(_nyPizzaStorePizzaBuilder.Object);
+
+            _nyPizzaStorePizzaBuilder
+                .Setup(x => x.AddOnions())
+                .Returns(_nyPizzaStorePizzaBuilder.Object);
+
+            _nyPizzaStorePizzaBuilder
+                .Setup(x => x.BuildPizza())
+                .Returns(mockPizza.Object);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Follow_Expected_Logic()
+        {
+            var sut = new OrderNyStylePizzaExample(_nyPizzaStorePizzaBuilder.Object);
+            sut.PlayOrderPizzaExample();
+        }
+    }
+}
